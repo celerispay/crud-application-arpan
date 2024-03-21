94465ebe773077a9597192a5259fe714a2470dd2
unknown <arpan@celerispay.com>
2024-03-21 18:10:01 +0530

    spring batch implementation

--- src/main/resources/db/migration/V2__AddUser.sql
+++ src/main/resources/db/migration/V2__AddUser.sql
@@ -1,5 +1,5 @@
 -- V2__AddUser.sql
 -- Insert initial data
 INSERT INTO User (email, username, password, role)
-VALUES ('arpan@gmail.com', 'arpan','$2a$12$yW5PL.Ilrtsb/HAPHcDUouAFABgtsdRBjdXZt7Bz8OWqvcoJON73q', 'ROLE_NORMAL'),
-       ('roshani@gmail.com', 'tapan', '$2a$12$o8lEnn9VMWzpNnnFbM1gfe/WA83GFw8RIFbjeq6AjW2zcDZOudELW', 'ROLE_ADMIN');
+VALUES ('arpan@gmail.com', 'arpan','$2a$12$xwPoHyG6N0XTxDKMgW3/EubcrSagTtLin0SrB20Lh4e3ymmINIT3W', 'ROLE_NORMAL'),
+       ('roshani@gmail.com', 'tapan', '$2a$12$hqMsMM3Wx3go/JHe.rbT4OpJWyxKIHgjxRnTs.GQPRedo9SELLcUS', 'ROLE_ADMIN');
---
+++ src/test/java/com/crudapplication/repository/StudentRepoTests.java
@@ -0,0 +1,111 @@
+package com.crudapplication.repository;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import javax.persistence.EntityManager;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import com.crudapplication.entity.College;
+import com.crudapplication.entity.Director;
+import com.crudapplication.entity.Student;
+import com.crudapplication.service.StudentService;
+
+
+@DataJpaTest
+@ExtendWith(MockitoExtension.class)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class StudentRepoTests {
+	
+	@Autowired
+	private StudentRepo repository;
+	
+	private Student student;
+	
+	@BeforeEach
+	public void setupTestData() {
+		student=Student.builder()
+				.id(1)
+				.name("Arpan")
+				.marks("97")
+				.college(College.builder()
+						.id(1901)
+						.name("MVM")
+						.place("Lucknow")
+						.director(Director.builder()
+								.id(1)
+								.name("Mohan Lal").build()).build())
+				.build();
+	}
+	
+	
+	@Test
+    public void testFindAll() {
+		Student studentOne=Student.builder()
+				.id(2)
+				.name("Sakshi")
+				.marks("97")
+				.college(College.builder()
+						.id(1901)
+						.name("MVM")
+						.place("Lucknow")
+						.director(Director.builder()
+								.id(1)
+								.name("Mohan Lal").build()).build())
+				.build();
+		
+		Student studentTwo=Student.builder()
+				.id(1)
+				.name("Rahul")
+				.marks("97")
+				.college(College.builder()
+						.id(1901)
+						.name("MVM")
+						.place("Lucknow")
+						.director(Director.builder()
+								.id(1)
+								.name("Mohan Lal").build()).build())
+				.build();
+		
+		repository.save(studentOne);
+		repository.save(studentTwo);
+		
+		List<Student> students=(List<Student>) repository.findAll();
+		assertEquals(students.isEmpty(),false);
+//		assertEquals(students.size(),6);
+    }
+	  
+	@Test
+	public void testGetStudent() {
+		repository.save(student);
+		Student students=repository.findById(student.getId()).get();
+		assertNotNull(students);
+	}
+	
+	@Test
+	public void testAddStudent() {
+		Student addedStudent=repository.save(student);
+		assertNotNull(addedStudent);
+		assertEquals(true, addedStudent.getId() > 0);
+
+	}
+	
+	
+	
+	
+
+}
